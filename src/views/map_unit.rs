176f3fd84678d6c0812e265cc3f62a178af0ccf6
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-10-10 21:54:24 -0400

    fix id overlap bug

--- src/views/map_unit.rs
+++ src/views/map_unit.rs
@@ -79,100 +79,108 @@ pub fn update(
                     builder.leaf(node + 1, format!("X: {}", event.x));
                     builder.leaf(node + 2, format!("Y: {}", event.y));
                     if builder.dir(node + 3, "Pages") {
-                        let node = node << 2;
-                        for page in &event.pages {
-                            // TODO: trigger term
+                        for (index, page) in event.pages.iter().enumerate() {
+                            let node = (node + 4 + index as u64) << 8;
+                            if builder.dir(node, format!("Page {}", index + 1)) {
+                                // TODO: trigger term
 
-                            if builder.dir(node + 2, "Graphic") {
+                                if builder.dir(node + 2, "Graphic") {
+                                    builder.leaf(
+                                        node + 3,
+                                        format!(
+                                            "File: {}",
+                                            encoding.to_encoding().decode(&page.graphic.file).0
+                                        ),
+                                    );
+                                    builder
+                                        .leaf(node + 4, format!("Index: {}", page.graphic.index));
+                                    builder.leaf(
+                                        node + 5,
+                                        format!("Direction: {}", page.graphic.direction),
+                                    );
+                                    builder.leaf(
+                                        node + 6,
+                                        format!("Pattern: {}", page.graphic.pattern),
+                                    );
+                                    builder.leaf(
+                                        node + 7,
+                                        format!("Transparent: {}", page.graphic.transparent),
+                                    );
+                                }
+                                builder.close_dir();
+
+                                if builder.dir(node + 8, "Movement") {
+                                    builder
+                                        .leaf(node + 9, format!("Type: {}", page.movement.r#type));
+                                    builder.leaf(
+                                        node + 10,
+                                        format!("Frequency: {}", page.movement.frequency),
+                                    );
+                                    builder
+                                        .leaf(node + 11, format!("Speed: {}", page.movement.speed));
+                                    // builder.leaf(node + 12, format!("Route: {}", page.movement.route));
+                                }
+                                builder.close_dir();
+
                                 builder.leaf(
-                                    node + 3,
+                                    node + 13,
                                     format!(
-                                        "File: {}",
-                                        encoding.to_encoding().decode(&page.graphic.file).0
+                                        "Trigger: {}",
+                                        match page.trigger {
+                                            Trigger::ActionButton => "Action Button",
+                                            Trigger::PlayerTouch => "Player Touch",
+                                            Trigger::EventTouch => "Event Touch",
+                                            Trigger::Autorun => "Autorun",
+                                            Trigger::Parallel => "Parallel process",
+                                        }
                                     ),
                                 );
-                                builder.leaf(node + 4, format!("Index: {}", page.graphic.index));
                                 builder.leaf(
-                                    node + 5,
-                                    format!("Direction: {}", page.graphic.direction),
+                                    node + 14,
+                                    format!(
+                                        "Priority: {}",
+                                        match page.priority {
+                                            Priority::BelowCharacters => "Below Characters",
+                                            Priority::SameAsCharacters => "Same as Characters",
+                                            Priority::AboveCharacters => "Above Characters",
+                                        }
+                                    ),
                                 );
-                                builder
-                                    .leaf(node + 6, format!("Pattern: {}", page.graphic.pattern));
                                 builder.leaf(
-                                    node + 7,
-                                    format!("Transparent: {}", page.graphic.transparent),
+                                    node + 15,
+                                    format!("Forbid Event Overlap: {}", page.forbid_event_overlap),
                                 );
-                            }
-                            builder.close_dir();
-
-                            if builder.dir(node + 8, "Movement") {
-                                builder.leaf(node + 9, format!("Type: {}", page.movement.r#type));
                                 builder.leaf(
-                                    node + 10,
-                                    format!("Frequency: {}", page.movement.frequency),
+                                    node + 16,
+                                    format!(
+                                        "Animation Type: {}",
+                                        match page.animation_type {
+                                            AnimationType::Standing => "Standing Animation",
+                                            AnimationType::Walking => "Walking Animation",
+                                            AnimationType::DirectionFixInanimated =>
+                                                "Direction Fix/Inanimated",
+                                            AnimationType::DirectionFixAnimated =>
+                                                "Direction Fix/Animated",
+                                            AnimationType::FixedGraphic => "Fixed Graphic",
+                                            AnimationType::Spin => "Spin",
+                                        }
+                                    ),
                                 );
-                                builder.leaf(node + 11, format!("Speed: {}", page.movement.speed));
-                                // builder.leaf(node + 12, format!("Route: {}", page.movement.route));
-                            }
-                            builder.close_dir();
-
-                            builder.leaf(
-                                node + 13,
-                                format!(
-                                    "Trigger: {}",
-                                    match page.trigger {
-                                        Trigger::ActionButton => "Action Button",
-                                        Trigger::PlayerTouch => "Player Touch",
-                                        Trigger::EventTouch => "Event Touch",
-                                        Trigger::Autorun => "Autorun",
-                                        Trigger::Parallel => "Parallel process",
+                                if builder.dir(node + 17, "Commands") {
+                                    let node = node << 16;
+                                    for (index, command) in page.commands.iter().enumerate() {
+                                        builder.leaf(
+                                            node + index as u64,
+                                            format!(
+                                                "{index}: {}{:?} {}",
+                                                "\t".repeat(command.indent as usize),
+                                                command.instruction,
+                                                encoding.to_encoding().decode(&command.string).0,
+                                            ),
+                                        )
                                     }
-                                ),
-                            );
-                            builder.leaf(
-                                node + 14,
-                                format!(
-                                    "Priority: {}",
-                                    match page.priority {
-                                        Priority::BelowCharacters => "Below Characters",
-                                        Priority::SameAsCharacters => "Same as Characters",
-                                        Priority::AboveCharacters => "Above Characters",
-                                    }
-                                ),
-                            );
-                            builder.leaf(
-                                node + 15,
-                                format!("Forbid Event Overlap: {}", page.forbid_event_overlap),
-                            );
-                            builder.leaf(
-                                node + 16,
-                                format!(
-                                    "Animation Type: {}",
-                                    match page.animation_type {
-                                        AnimationType::Standing => "Standing Animation",
-                                        AnimationType::Walking => "Walking Animation",
-                                        AnimationType::DirectionFixInanimated =>
-                                            "Direction Fix/Inanimated",
-                                        AnimationType::DirectionFixAnimated =>
-                                            "Direction Fix/Animated",
-                                        AnimationType::FixedGraphic => "Fixed Graphic",
-                                        AnimationType::Spin => "Spin",
-                                    }
-                                ),
-                            );
-                            if builder.dir(node + 17, "Commands") {
-                                let node = node << 16;
-                                for (index, command) in page.commands.iter().enumerate() {
-                                    builder.leaf(
-                                        node + index as u64,
-                                        format!(
-                                            "{index}: {}{:?} {}",
-                                            "\t".repeat(command.indent as usize),
-                                            command.instruction,
-                                            encoding.to_encoding().decode(&command.string).0,
-                                        ),
-                                    )
                                 }
+                                builder.close_dir();
                             }
                             builder.close_dir();
                         }
